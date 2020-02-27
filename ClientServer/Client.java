67385d7ea752cbef375953ac62cce51d8c799001
mkumarj786 <61533525+mkumarj786@users.noreply.github.com>
2020-02-27 12:04:39 +0530

    Add files via upload

---
+++ ClientServer/Client.java
@@ -0,0 +1,223 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.awt.Color;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.lang.Thread;
+import java.util.concurrent.*;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+
+
+
+
+public class Client extends javax.swing.JFrame {
+
+    //variables
+    String server; 
+    private javax.swing.JTextArea msg_area;
+    private javax.swing.JButton send;
+    public javax.swing.JLabel status;
+    private javax.swing.JLabel img;
+    private javax.swing.JTextField msg;
+    private javax.swing.JLabel po;
+    private javax.swing.JTextField port_number;
+    private javax.swing.JButton connect;
+    
+    //socket vari
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private String message="";
+    public Socket s;
+    //public boolean on = false;
+
+
+    //main function
+public static void main(String[] args) {
+    Client c = new Client("127.0.0.1");
+    ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+ 
+        Runnable task = new Runnable() {
+            public void run() {
+                
+                c.start();
+            }
+        };
+ // delay time is 10. so make sure you open the server and press the connect button in client part within 10 seconds. 
+ //after 10 seconds you need to restart the client.
+        int delay = 10;
+        scheduler.schedule(task, delay, TimeUnit.SECONDS);
+        scheduler.shutdown();
+    
+}
+   
+
+
+    public Client(String s){
+
+        component();
+        
+        this.setTitle("Client");
+        this.setVisible(true);
+        server = s;
+    }
+
+   //component 
+    private void component() {
+
+         po = new javax.swing.JLabel("Enter port number");
+         port_number = new javax.swing.JTextField();
+         connect = new javax.swing.JButton("Connect");
+         msg = new javax.swing.JTextField();
+         status = new javax.swing.JLabel("waiting for connection");
+         img = new javax.swing.JLabel();
+         send = new javax.swing.JButton("send");
+         msg_area = new javax.swing.JTextArea();
+
+         img.setIcon(new javax.swing.ImageIcon(getClass().getResource("/background.png")));
+        status.setForeground(Color.WHITE);
+        po.setForeground(Color.WHITE);
+        status.setForeground(Color.WHITE);
+       
+        
+
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        img.setBounds(0, 0, 500, 500);
+        po.setBounds(30,20,270,25);
+        port_number.setBounds(30,50,80,30);
+        connect.setBounds(130,50,120,30);
+        msg.setBounds(30,100,270,30);
+        send.setBounds(310, 100, 80, 30);
+        status.setBounds(30,130,270,25);
+        msg_area.setBounds(30,170,270,270);
+        msg_area.setEditable(false);
+        msg_area.setColumns(20);
+        msg_area.setRows(5);
+
+
+        send.addActionListener(new java.awt.event.ActionListener(){
+            public void actionPerformed(java.awt.event.ActionEvent evt){
+                sendaction(evt);
+            }
+       });
+
+        connect.addActionListener(new java.awt.event.ActionListener(){
+           public void actionPerformed(java.awt.event.ActionEvent e){
+               connectaction(e);
+           }
+       });
+
+       
+
+        add(msg);add(po);
+       
+        add(connect);
+        add(port_number);
+        add(msg_area);add(send);add(status);
+        setLayout(null);
+        setSize(500,500);
+        add(img);
+
+
+    }
+
+    //actions
+
+public void connectaction(java.awt.event.ActionEvent e){
+    
+    status.setText("trying to connect");
+    //on = true;   
+     port_number.setEditable(false);  
+    
+
+}
+
+private void sendaction(java.awt.event.ActionEvent evt){
+    SendMessage(msg.getText());
+    msg.setText("");
+    
+}
+
+
+//start function (connect part)
+    public void start()
+    {             
+        if(port_number.getText().equals("")){
+            port_number.setText("5000");
+            status.setText("Trying to connect to default port : 5000");
+        }
+        try{ 
+           
+            try{
+            s = new Socket(InetAddress.getByName(server),Integer.parseInt(port_number.getText()));
+            
+            }catch(IOException ioException){
+            status.setText("sever is not online");
+            JOptionPane.showMessageDialog(null, "sever is not online", "warning", JOptionPane.WARNING_MESSAGE);
+            dispose();    
+        }
+    
+            status.setText("connected to I.P :"+InetAddress.getByName(server) );   
+
+            out = new ObjectOutputStream(s.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(s.getInputStream());
+
+            chat();
+        
+            
+        }catch(IOException ioException)
+        {   
+             ioException.printStackTrace();
+        }}
+        
+
+        
+    
+//chat function
+    private void chat() throws IOException
+    {
+        do{
+
+                try{
+
+                    message = (String) in.readObject();
+                    msg_area.append("\n"+message);    
+                }catch(ClassNotFoundException classNotFoundException){}
+
+        }
+        while(!message.equals("Client : end"));
+    }
+
+    //send message function 
+
+    private void SendMessage(String mes){
+
+        try{
+            out.writeObject("Client : " + mes);
+            out.flush();
+            msg_area.append("\nClient : " + mes);
+        }catch(IOException ioException)
+        {
+            msg_area.append("\n Unable to Send Message");
+        }
+    }
+
+
+
+    
+
+
+
+
+}
---
+++ ClientServer/Server.java
@@ -0,0 +1,170 @@
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.awt.Color;
+import java.io.ObjectOutputStream;
+import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+
+
+public class Server extends javax.swing.JFrame{
+
+//variables
+private JTextField msg;
+private JButton send;
+private JTextArea msg_area;
+private JLabel img;
+
+//server variables
+private ServerSocket ss;
+private Socket s;
+private JLabel l;
+private ObjectInputStream in;
+private ObjectOutputStream out;
+private int port = 5000;
+private int clients = 50;
+
+ //main function
+public static void main(String[] args) {
+    
+    Server server = new Server();
+    server.run();
+    
+}
+
+
+
+public Server(){
+
+Component();
+this.setVisible(true);
+this.setTitle("Server");
+
+}
+
+
+//component
+private void Component(){
+
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setResizable(false);
+
+    img = new javax.swing.JLabel();
+    msg = new javax.swing.JTextField();
+    send = new javax.swing.JButton("Send");
+    msg_area = new javax.swing.JTextArea();
+    l = new javax.swing.JLabel("Waiting for client to connect. PORT : 5000");
+
+
+    img.setIcon(new javax.swing.ImageIcon(getClass().getResource("/background.png")));
+    l.setForeground(Color.WHITE);
+   
+    img.setBounds(0, 0, 500, 500);
+    l.setBounds(30,20,370,30);
+    msg.setBounds(30,50,270,30);
+    send.setBounds(310, 50, 80, 30);
+    msg_area.setBounds(30,150,270,270);
+    msg_area.setEditable(false);
+
+    
+    send.addActionListener(new java.awt.event.ActionListener(){
+        public void actionPerformed(java.awt.event.ActionEvent evt){
+
+            sendaction(evt);
+        }
+    });
+
+    add(send);add(msg);add(msg_area);add(l);
+    add(img);
+    setSize(500,500);
+    setLayout(null);
+
+
+}
+
+//actions
+public void sendaction(java.awt.event.ActionEvent evt){
+
+    SendMessage(msg.getText());
+    msg.setText("");
+    
+}
+
+//run function (server accept)
+public void run(){
+    try{
+
+        
+
+            ss= new ServerSocket(port,clients);
+            while(true){
+                try{
+                    s = ss.accept();
+                    l.setText("connected to Client on port '5000'");
+                    out = new ObjectOutputStream(s.getOutputStream());
+                    out.flush();
+                    in = new ObjectInputStream(s.getInputStream());
+
+                    chat();
+                }catch(EOFException eofException)
+                {
+                }
+
+            }
+
+
+        
+
+    }catch(IOException ioException)
+    {
+            ioException.printStackTrace();
+    }
+
+
+}
+
+
+//chat function
+private void chat()throws IOException
+{
+
+    String message = "";
+    do{
+
+        try{
+                message = (String) in.readObject();
+                msg_area.append("\n"+message);
+
+        }catch(ClassNotFoundException classNotFoundException){}
+
+    }while(!message.equals("Client : end"));
+
+}
+
+
+//send message
+private void SendMessage(String message){
+
+        try{
+                out.writeObject("Server : " + message);
+                out.flush();
+                msg_area.append("\nServer : " + message);
+
+      }catch(IOException ioException){
+            msg_area.append("\n client not connected");
+
+      }
+
+}
+
+
+
+
+}
